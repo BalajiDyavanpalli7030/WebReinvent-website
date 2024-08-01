a8fb9344a2c4b9a534a997c5d9a5510c13a292d7
Balaji Vyankatesh Dyavanpalli <62424558+BalajiDyavanpalli7030@users.noreply.github.com>
2024-08-01 16:48:25 +0530

    Update api.ts

--- src/services/api.ts
+++ src/services/api.ts
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const API_BASE_URL = 'https://reqres.in/api';
 
-// Create an Axios instance with default settings
+
 const api = axios.create({
   baseURL: API_BASE_URL,
   headers: {
@@ -10,7 +10,7 @@ const api = axios.create({
   },
 });
 
-// Function to handle user sign-in
+
 export const signIn = async (email: string, password: string) => {
   try {
     const response = await api.post('/login', { email, password });
@@ -20,32 +20,12 @@ export const signIn = async (email: string, password: string) => {
   }
 };
 
-// Function to handle user sign-up
+
 export const signUp = async (email: string, password: string) => {
   try {
     const response = await api.post('/register', { email, password });
     return response.data;
   } catch (error) {
     throw new Error('Sign Up failed');
-  }
-};
-
-// Function to get a list of users
-export const fetchUsers = async () => {
-  try {
-    const response = await api.get('/users');
-    return response.data;
-  } catch (error) {
-    throw new Error('Failed to fetch users');
-  }
-};
-
-// Function to get a single user by ID
-export const fetchUserById = async (id: number) => {
-  try {
-    const response = await api.get(`/users/${id}`);
-    return response.data;
-  } catch (error) {
-    throw new Error('Failed to fetch user');
   }
 };
